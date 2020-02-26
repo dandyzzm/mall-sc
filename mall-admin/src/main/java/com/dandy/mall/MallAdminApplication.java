049f6108682667d10d5e71b3cfe5c8424bceb47d
dandy <dandy_zzm@163.com>
2020-02-26 13:17:21 +0800

    exclude DataSourceAutoConfiguration

--- mall-admin/src/main/java/com/dandy/mall/MallAdminApplication.java
+++ mall-admin/src/main/java/com/dandy/mall/MallAdminApplication.java
@@ -3,6 +3,8 @@ package com.dandy.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -10,7 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * Created by macro on 2018/4/26.
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 @MapperScan("com.dandy.mall.dao")
 public class MallAdminApplication {
     public static void main(String[] args) {
--- mall-portal/src/main/java/com/dandy/mall/portal/MallPortalApplication.java
+++ mall-portal/src/main/java/com/dandy/mall/portal/MallPortalApplication.java
@@ -3,6 +3,8 @@ package com.dandy.mall.portal;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -10,7 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * Created by macro on 2018/4/26.
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 @MapperScan("com.dandy.mall.portal.dao")
 public class MallPortalApplication {
     public static void main(String[] args) {
